6a3db67ac996ac31885376eaff29809df64e8a31
YOrtiz <yortiz@blumonpay.com.mx>
2023-11-19 16:32:01 -0600

    Versión 5.0.0 - °Se agrega seccion blog y testimoniales y se termina el layout principal

--- src/components/HomeData.jsx
+++ src/components/HomeData.jsx
@@ -49,12 +49,12 @@ function HomeData() {
         <div className='advertisements-container'>
           <div className='advertisements'>
             <picture>
-              <source srcset='/build/img/anuncio1.avif' type='image/avif' />
-              <source srcset='/build/img/anuncio1.webp' type='image/webp' />
+              <source srcSet='/build/img/anuncio1.avif' type='image/avif' />
+              <source srcSet='/build/img/anuncio1.webp' type='image/webp' />
               <img
                 loading='lazy'
                 width='200'
-                hright='300'
+                height='300'
                 src='/build/img/anuncio1.jpg'
                 alt='anuncuio'
               />
@@ -102,12 +102,12 @@ function HomeData() {
 
           <div className='advertisements'>
             <picture>
-              <source srcset='/build/img/anuncio2.avif' type='image/avif' />
-              <source srcset='/build/img/anuncio2.webp' type='image/webp' />
+              <source srcSet='/build/img/anuncio2.avif' type='image/avif' />
+              <source srcSet='/build/img/anuncio2.webp' type='image/webp' />
               <img
                 loading='lazy'
                 width='200'
-                hright='300'
+                height='300'
                 src='/build/img/anuncio2.jpg'
                 alt='anuncuio'
               />
@@ -152,12 +152,12 @@ function HomeData() {
 
           <div className='advertisements'>
             <picture>
-              <source srcset='/build/img/anuncio3.avif' type='image/avif' />
-              <source srcset='/build/img/anuncio3.webp' type='image/webp' />
+              <source srcSet='/build/img/anuncio3.avif' type='image/avif' />
+              <source srcSet='/build/img/anuncio3.webp' type='image/webp' />
               <img
                 loading='lazy'
                 width='200'
-                hright='300'
+                height='300'
                 src='/build/img/anuncio3.jpg'
                 alt='anuncuio'
               />
@@ -207,6 +207,91 @@ function HomeData() {
           </Link>
         </div>
       </section>
+
+      <section className='contact-img'>
+        <h2>Encuentra la casa de tus sueños</h2>
+        <p>
+          Llena el formulario de contacto y un asesor se pondrá en contacto
+          contigo a la brevedad
+        </p>
+        <Link to='/data/contacto' className='yellow-button-inline'>
+          Contactános
+        </Link>
+      </section>
+
+      <div className='container section under-section'>
+        <section className='blog'>
+          <h3>Nuestro Blog</h3>
+          <article className='enter-blog'>
+            <div className='image'>
+              <picture>
+                <source srcSet='/build/img/blog1.avif' type='image/avif' />
+                <source srcSet='/build/img/blog1.webp' type='image/webp' />
+                <img
+                  loading='lazy'
+                  width='200'
+                  height='300'
+                  src='/build/img/blog1.jpg'
+                  alt='imagen blog'
+                />
+              </picture>
+            </div>
+
+            <div className='enter-text'>
+              <Link to='/data/contacto'>
+                <h4>Terraza en el techo de tu casa</h4>
+                <p>
+                  Escrito el: <span>18/11/2023</span> por: <span>Admin</span>
+                </p>
+                <p>
+                  Consejos para construir una terraza en el techo de tu casa con
+                  los mejores materiales y ahorrando dinero.
+                </p>
+              </Link>
+            </div>
+          </article>
+
+          <article className='enter-blog'>
+            <div className='image'>
+              <picture>
+                <source srcSet='/build/img/blog2.avif' type='image/avif' />
+                <source srcSet='/build/img/blog2.webp' type='image/webp' />
+                <img
+                  loading='lazy'
+                  width='200'
+                  height='300'
+                  src='/build/img/blog2.jpg'
+                  alt='imagen blog'
+                />
+              </picture>
+            </div>
+
+            <div className='enter-text'>
+              <Link to='/data/contacto'>
+                <h4>Guía para la decoración de tu hogar</h4>
+                <p>
+                  Escrito el: <span>18/11/2023</span> por: <span>Admin</span>
+                </p>
+                <p>
+                  Maximiza el espacio en tu hogar con esta guía, aprende a
+                  combinar muebles y colores para darle vida a tu espacio
+                </p>
+              </Link>
+            </div>
+          </article>
+        </section>
+
+        <section className='testimonials'>
+          <h3>Testimoniales</h3>
+          <div className='testimonial'>
+            <blockquote>
+              El personal se comportó de excelente forma, muy buena atención y
+              la casa que me ofrecieron cumple todas mis expectativas.
+            </blockquote>
+            <p>- Yahir Ortiz</p>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
